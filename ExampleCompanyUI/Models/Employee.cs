38257fd12bb62098a96343338cd9ad373c837b1e
lucasgkingston <Lucas.G.Kingston@gmail.com>
2020-05-30 13:07:40 -0400

    init project

---
+++ ExampleCompanyUI/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExampleCompanyUI.Models
+{
+    public class Employee
+    {
+        int ID { get; set; }
+        string FirstName { get; set; }
+        string LastName { get; set; }
+
+    }
+}
